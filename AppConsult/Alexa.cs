e043b484c2fa1f8e2759f978d94864317f9d411b
Tyler Hall <jamesthall90@gmail.com>
2019-12-20 09:52:35 -0500

    Initial Skill creation

--- AppConsult/Alexa.cs
+++ AppConsult/Alexa.cs
@@ -1,12 +1,19 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Xml;
+using Alexa.NET;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Microsoft.SyndicationFeed.Rss;
 using Newtonsoft.Json;
 
 namespace AppConsult
@@ -15,22 +22,63 @@ namespace AppConsult
     {
         [FunctionName("Alexa")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
-            HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            var json = await req.ReadAsStringAsync();
+            var skillRequest = JsonConvert.DeserializeObject<SkillRequest>(json);
 
-            string name = req.Query["name"];
+            var requestType = skillRequest.GetRequestType();
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            SkillResponse response = null;
 
-            return name != null
-                       ? (ActionResult) new OkObjectResult($"Hello, {name}")
-                       : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
-            
+            if (requestType == typeof(LaunchRequest))
+            {
+                response = ResponseBuilder.Tell("Welcome to AppConsult!");
+                response.Response.ShouldEndSession = false;
+            }
+
+            else if (requestType == typeof(IntentRequest))
+            {
+                var intentRequest = skillRequest.Request as IntentRequest;
+
+                if (intentRequest?.Intent.Name != "LastPosts")
+                {
+                    return new OkObjectResult((SkillResponse) null);
+                }
+
+                const string rss = "https://blogs.msdn.microsoft.com/appconsult/feed/";
+
+                var news = await ParseFeed(rss);
+
+                var output = $"The title of the last article is {news.FirstOrDefault()}";
+
+                response = ResponseBuilder.Tell(output);
+            }
+
+            return new OkObjectResult(response);
+        }
+
+        private static async Task<List<string>> ParseFeed(string url)
+        {
+            var news = new List<string>();
+
+            using (var xmlReader = XmlReader.Create(url, new XmlReaderSettings { Async = true }))
+            {
+                var feedReader = new RssFeedReader(xmlReader);
+                while (await feedReader.Read())
+                {
+                    if (feedReader.ElementType != Microsoft.SyndicationFeed.SyndicationElementType.Item)
+                    {
+                        continue;
+                    }
+
+                    var item = await feedReader.ReadItem();
+                    news.Add(item.Title);
+                }
+            }
+
+            return news;
         }
     }
 }
